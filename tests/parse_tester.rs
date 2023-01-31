66570ab80c3cca037d4a87b5cc37223c151fbc9b
Peter Krull <peterkrullpeter@gmail.com>
2023-01-31 17:25:13 +0100

    Added more extensive unit tests

--- tests/parse_tester.rs
+++ tests/parse_tester.rs
@@ -2,26 +2,79 @@
 mod tests {
     use hex_literal::hex;
 
+    const RAW_BYTES : [u8;25] =
+        hex!("0F E0 03 1F 58 C0 07 16 B0 80 05 2C 60 01 0B F8 C0 07 00 00 00 00 00 03 00");
+
+    const DECODED_PACKET : sbus::SBusPacket = sbus::SBusPacket {
+        channels: [992, 992, 352, 992, 352, 352, 352, 352, 352, 352, 992, 992, 0, 0, 0, 0],
+        d1: true,
+        d2: true,
+        failsafe: false,
+        frame_lost: false
+    };
+
+
     #[test]
-    fn test_sbus_parse() {
+    fn test_sbus_parse_basic() {
 
         let mut parser = sbus::SBusPacketParser::new();
 
-        let bytes =
-            hex!("00 0F E0 03 1F 58 C0 07 16 B0 80 05 2C 60 01 0B F8 C0 07 00 00 00 00 00 03 00");
+        // Push full message
+        parser.push_bytes(&RAW_BYTES);
 
-        parser.push_bytes(&bytes);
-        let parsed = parser.try_parse().unwrap();
+        assert_eq!( Some( DECODED_PACKET ), parser.try_parse() );
+    }
 
-        assert_eq!(
-            sbus::SBusPacket {
-                channels: [992, 992, 352, 992, 352, 352, 352, 352, 352, 352, 992, 992, 0, 0, 0, 0],
-                d1: true,
-                d2: true,
-                failsafe: false,
-                frame_lost: false
-            },
-            parsed
-        );
+    #[test]
+    fn test_sbus_parse_with_shifting() {
+
+        let mut parser = sbus::SBusPacketParser::new();
+        
+        // Push message within other garbage bytes
+        parser.push_bytes(&RAW_BYTES[5..15]);
+        parser.push_bytes(&RAW_BYTES); // Actual message
+        parser.push_bytes(&RAW_BYTES[5..15]);
+
+        assert_eq!( Some( DECODED_PACKET ), parser.try_parse() );
+    }
+
+    #[test]
+    fn test_sbus_error_missing_byte() {
+
+        let mut parser = sbus::SBusPacketParser::new();
+        
+        // Push bytes with one missing (and some extra bytes)
+        parser.push_bytes(&RAW_BYTES[0..7]);
+        parser.push_bytes(&RAW_BYTES[8..25]);
+        parser.push_bytes(&RAW_BYTES[8..10]);
+
+        assert_eq!( None, parser.try_parse() );
+    }
+
+    #[test]
+    fn test_sbus_error_too_short() {
+
+        let mut parser = sbus::SBusPacketParser::new();
+        
+        // Push message that is too short
+        parser.push_bytes(&RAW_BYTES[0..23]);
+        
+        assert_eq!( None, parser.try_parse() );
+    }
+
+    #[test]
+    fn test_sbus_parse_two_parter() {
+
+        let mut parser = sbus::SBusPacketParser::new();
+        
+        // Push first bytes of message
+        parser.push_bytes(&RAW_BYTES[0..10]);
+
+        assert_eq!( None, parser.try_parse() );
+
+        // Push remaining message
+        parser.push_bytes(&RAW_BYTES[10..25]);
+
+        assert_eq!( Some(DECODED_PACKET), parser.try_parse() );
     }
 }
